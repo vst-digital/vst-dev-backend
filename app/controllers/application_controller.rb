0474318d6aae73a77708e5046f21531433b89b39
togulsharan <togulsharan@gmail.com>
2022-05-16 15:56:45 +1000

    credentials fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
     # before_action :current_project, only: %i[ show index update destroy ]
 
     def set_current_user
-        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.jwt_secret_key).first
         user_id = jwt_payload['sub']
         User.find(user_id.to_s)
     end
