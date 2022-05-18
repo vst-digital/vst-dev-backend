eb77e37b9b78d7481ab80bbc7ca82bf650f10176
togulsharan <togulsharan@gmail.com>
2022-05-19 09:28:15 +1000

    cleared host in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.hosts << 'https://www.vstapp.co.za/api'
+    config.hosts.clear
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
