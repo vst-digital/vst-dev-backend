bee82ae32de9ced5fe7098424c3473fc87e08fa7
togulsharan <togulsharan@gmail.com>
2022-06-14 15:17:29 +1000

    updates in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: "toro@gmail.com", first_name: "Toro", last_name: "Singh", contact: "9876543210", role: "subscription_owner", password: "123123123", password_confirmation: "123123123")
